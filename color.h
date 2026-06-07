145924e0608615d41c696787da1dea3797986de3
AbenezerA <abitad2015@gmail.com>
2026-06-07 18:13:36 -0400

    Add color, ray, and vec3 class libs

---
+++ color.h
@@ -0,0 +1,26 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+// alias
+using color = vec3;
+
+void write_color(std::ostream& out, const color& pixel_color) {
+    // rgb values correspond to xyz of vec3
+    auto r = pixel_color.x();
+    auto g = pixel_color.y();
+    auto b = pixel_color.z();
+
+    // Scale from [0, 1] to [0, 255] range
+    int rbyte = int(255.999 * r);
+    int gbyte = int(255.999 * g);
+    int bbyte = int(255.999 * b);
+
+    // Write out the pixel color components.
+    out << rbyte << ' ' << gbyte << ' ' << bbyte << '\n';
+}
+
+#endif
